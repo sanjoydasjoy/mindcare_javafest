78bc41ea7db8d815b562f8cf7596f3f0098434bb
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-23 05:37:23 +0600

    Add files via upload

---
+++ server/src/main/java/com/mindcare/MindCareApplication.java
@@ -0,0 +1,11 @@
+package com.mindcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindCareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MindCareApplication.class, args);
+    }
+}
