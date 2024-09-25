522252e97a68023b8ed80fa32b6f1bd435c0e7c9
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-25 21:43:51 +0600

    Delete server directory

--- server/src/main/java/com/mindcare/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/mindcare/backend/repository/UserRepository.java
-package com.mindcare.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mindcare.backend.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
